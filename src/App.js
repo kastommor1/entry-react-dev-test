b2eca6afbe27983a82a1239979d4efc965d96d15
kastommor1 <kastommor@gmail.com>
2022-06-01 19:49:34 +0300

    Routes from index to App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,60 @@
 import React from "react";
-import { widthQuery } from "./HOCs/HOCs"
-import { Outlet, Routes, Route, Navigate } from "react-router-dom";
-import Header from "./components/App/Header";
-import PropTypes from "prop-types";
+
+//routes
+import { Routes, Route, Navigate } from "react-router-dom";
+import CategoryHOC from "./routes/Category";
+
+//apollo
 import { GET_CATEGORIES_NAME } from "./apollo-client/queries";
-import {setCategoriesName} from './data';
+
+//data
+import { widthQuery } from "./HOCs/HOCs"
+import { setCategoriesName } from './data';
+import PropTypes from "prop-types";
+
+//components
+import Header from "./components/App/Header";
 import WarningMessage from "./components/Warning-message";
 
-class App extends React.Component{
-  
+
+class App extends React.Component {
+
   // static propTypes = {
   //   query: PropTypes.isRequired
   // }
 
-  render(){
-    const {loading, error , data} = this.props.query; 
+  render() {
+    const { loading, error, data } = this.props.query;
 
-    if(loading) return <p>Loading...</p>
-    if(error) return <p>Error {error.message}</p>
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error {error.message}</p>
 
-    if(data && data.categories) {
+    if (data && data.categories) {
       setCategoriesName(data.categories);
-      if (data.categories.length === 0) return  <WarningMessage><p>No categories</p></WarningMessage>; 
-      
-      return(
+      if (data.categories.length === 0) return <WarningMessage><p>No categories</p></WarningMessage>;
+
+      return (
         <div>
-          <Header categories={data.categories}/>
+          <Header categories={data.categories} />
           <main>
             <Routes>
-                <Route path='' element={<Navigate to={'/categories/' + data.categories[0].name} />} />                    
-            </Routes>             
-            <Outlet/>
-          </main>      
+              <Route path='*'>
+                <Route path='categories/:categoryName' element={<CategoryHOC />} />
+                <Route path='' element={<Navigate to={'/categories/' + data.categories[0].name} />} />
+
+                <Route path='*' element={
+                  <WarningMessage>
+                    <h2>404</h2>
+                    <p>Page not found</p>
+                  </WarningMessage>
+                } />
+              </Route>
+            </Routes>
+          </main>
         </div>
       )
     }
-  }  
+  }
 }
 
 const AppHOC = widthQuery(App, GET_CATEGORIES_NAME);
--- src/index.js
+++ src/index.js
@@ -3,43 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppHOC from './App';
 
-
 //apollo
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apollo-client/cache"
 
 //routes
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import CategoryHOC from './routes/Category';
-
-//components
-import WarningMessage from './components/Warning-message';
-
+import { BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='*' 
-          element={
-          <ApolloProvider client={client}>
-            <AppHOC />
-          </ApolloProvider>
-          }>
-
-          <Route path='categories/:categoryName' element={<CategoryHOC/>} />     
-          <Route path='*' element={
-            <WarningMessage>
-              <h2>404</h2>
-              <p>Page not found</p>
-            </WarningMessage>
-          } />          
-        </Route>
-      </Routes>      
+      <ApolloProvider client={client}>
+        <AppHOC />
+      </ApolloProvider>
     </BrowserRouter>
-        
   </React.StrictMode>
 );
 
