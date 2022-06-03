9e6aadf4f582274b28bfa63e9a8002af9761fca8
kastommor1 <kastommor@gmail.com>
2022-06-03 19:01:29 +0300

    add/remove button product

--- src/Components/Category/Product-card.js
+++ src/Components/Category/Product-card.js
@@ -6,15 +6,26 @@ class ProductCard extends React.Component {
 
     render(){
         const product = this.props.product;
-        const {id, name, inStock, gallery, prices, brand, quantity} = product;
+        const {id, name, inStock, gallery, prices, brand, quantity} = product;        
       
         return (
             <div style={{border: '1px solid black'}}>
                 <img src={gallery[0]} alt=""  style={{height: 150}}/>
                 <p>{brand} {name}</p>
                 <p><b>{prices[0].currency.symbol}{prices[0].amount}</b></p>
-                {quantity && <p><b style={{color: 'red'}}>In cart</b></p>}
-                <button onClick={()=>{this.props.onAddToCart(product)}}>Add to cart</button>                
+                {/* {quantity && <p><b style={{color: 'red'}}>In cart</b></p>} */}
+                {!quantity ? 
+                <button 
+                onClick={()=>{this.props.onAddToCart(product)}} 
+                style={{color: 'green'}}>
+                    Add                    
+                </button>:
+                <button 
+                onClick={()=>{this.props.onAddToCart(product)}} 
+                style={{color: 'red'}}>
+                    Remove                    
+                </button>}
+
             </div>
         )
     }
