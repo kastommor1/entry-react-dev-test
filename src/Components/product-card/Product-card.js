4f80facf9b2fcb6fb502a62306e0f4d0e952abb3
kastommor1 <kastommor@gmail.com>
2022-06-09 16:22:29 +0300

    AddButton

--- src/Components/product-card/Product-card.js
+++ src/Components/product-card/Product-card.js
@@ -1,39 +1,30 @@
 import React from "react";
 import '../../styles/product-card/Product-card.css'
-import cart from "../../data/Empty-Cart-White.svg";
+
 import Price from "./Price";
 import GalleryStock from "./Gallery-stock";
+import AddButton from "./Add-button";
 
 class ProductCard extends React.Component {
 
     render() {
-        const product = this.props.product;
-        const { id, name, inStock, gallery, prices, brand, quantity } = product;   
+        const { product, onAddToCart } = this.props;
+        const { id, name, inStock, gallery, prices, brand, quantity } = product;
 
         return (
-            <div className="product-card">               
+            <div className="product-card">
 
-                <GalleryStock inStock={inStock} gallery={gallery} name={name}/>
-                <p className="product-name" >{brand} {name}</p>
+                <GalleryStock inStock={inStock} gallery={gallery} name={name} />
+                
+                <p className="name" >{brand} {name}</p>
+
                 <Price prices={prices} />
 
-                {inStock && 
-                <div>
-                    {!quantity ?
-                        <button
-                            className="add-product-btn"
-                            onClick={() => { this.props.onAddToCart(product) }}
-                        >
-                            <img src={cart} alt="" />
-                        </button> :
-                        <button
-                            className="dell-product-btn"
-                            onClick={() => { this.props.onAddToCart(product) }}
-                        >
-                            <img src={cart} alt="" />
-                        </button>}
-                </div>}
-
+                <AddButton
+                    inStock={inStock}
+                    quantity={quantity}
+                    product={product}
+                    onAddToCart={onAddToCart} />
             </div>
         )
     }
