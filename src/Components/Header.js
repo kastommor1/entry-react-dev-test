8ccd0be4ba95055e77e7963793ac68ce98a00c08
kastommor1 <kastommor@gmail.com>
2022-06-09 17:39:06 +0300

    logo in the center

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
-import  "../styles/Header.css"
+import "../styles/Header.css"
 import Cart from "./Cart";
 import logo from "../data/a-logo.svg";
 
-class Header extends React.Component{
-    render(){        
+class Header extends React.Component {
+    render() {
         return (
             <header>
                 <nav>
                     <ul className="nav-links">
                         {this.props.categoriesName.map(
-                            category=>(
-                            <li key={category.name}>
-                                <NavLink 
-                                to={'/categories/'+category.name}> 
-                                    {category.name.toUpperCase()}
-                                </NavLink>
-                            </li>
-                            ))}                           
-                    </ul>                                       
+                            category => (
+                                <li key={category.name}>
+                                    <NavLink
+                                        to={'/categories/' + category.name}>
+                                        {category.name.toUpperCase()}
+                                    </NavLink>
+                                </li>
+                            ))}
+                    </ul>
                 </nav>
-                
-                <img src={logo} alt="logo"  className="logo"/>
-                
-                <button className="header-icon">$</button>                                   
-                <Cart cart = {this.props.cart}/>               
-             
+
+                <img src={logo} alt="logo" className="logo" />
+
+                <div className="header-icons">
+                    <button className="header-icon">$</button>
+                    <Cart cart={this.props.cart} />
+                </div>
+
+
+
             </header>
         )
     }
