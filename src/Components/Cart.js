9087756d8145c826791f70a6ff43c0a9bd051554
kastommor1 <kastommor@gmail.com>
2022-06-10 19:04:06 +0300

    filteredCart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import cart from "../data/Empty-Cart.svg";
+import cartIcon from "../data/Empty-Cart.svg";
 import "../styles/Cart.css"
 import ProductCardInCart from "./product-card/Product-card-in-cart";
 
@@ -16,25 +16,27 @@ class Cart extends React.Component{
         this.setState({showCart: !this.state.showCart});       
     }
 
-    render(){      
+    render(){
+        const filteredCart = this.props.cart.filter(product=>product.quantity > 0)         
+
         return(
             <div className="cart-mini">
                 <div onClick={this.showCart} className="cart-icon">
-                    <button className="header-icon"> <img src={cart} alt="fff" /></button>
-                    {this.props.cart.length > 0 && <b className="count-icon">{this.props.cart.length}</b>}
+                    <button className="header-icon"> <img src={cartIcon} alt="fff" /></button>
+                    {filteredCart.length > 0 && <b className="count-icon">{filteredCart.length}</b>}
                 </div>                  
                                   
                 <div               
                 className= {"cart-list" + (this.state.showCart ? ' cart-show' : '')}               
                 >
-                    <h2><b>My Bag,</b> {this.props.cart.length} items</h2>
+                    <h2><b>My Bag,</b> {filteredCart.length} items</h2>
 
-                    {this.props.cart.map(product=>(
+                    {filteredCart.map(product=>(
                         <ProductCardInCart 
                         key={product.id} 
                         product={product} 
                         onQuantityChange={this.props.onQuantityChange}
-                        onAttributeChange={this.props.onAttributeChange}                        
+                        onAttributeChange={this.props.onAttributeChange}                       
                         />
                     ))}
 
