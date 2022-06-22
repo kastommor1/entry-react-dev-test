fcd243f48ae1bff24f54d0c15d08f51eed0e1a2a
kastommor1 <kastommor@gmail.com>
2022-06-22 17:56:55 +0300

    fix apollo cache attributes

--- src/apollo-client/cache.js
+++ src/apollo-client/cache.js
@@ -1,7 +1,48 @@
-import { ApolloClient, InMemoryCache} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// const cache = new InMemoryCache()
+
+const cache = new InMemoryCache({
+    typePolicies: {
+        Product: {
+            fields: {
+                attributes: {
+                    merge(existing, incoming) {
+                        // return { ...existing, ...incoming };
+                        return { ...incoming };
+                    },
+                },
+            },
+        },
+
+        // AttributeSet: {
+        //     fields: {
+        //         items: {
+        //             merge(existing, incoming) {
+        //                 console.log('AttributeSet', existing, incoming);
+        //                 return { ...existing, ...incoming };
+        //                 // return incoming;
+        //             },
+        //         },
+        //     },
+        // },
+
+        // Attribute: {
+        //     fields: {
+        //         items: {
+        //             merge(existing, incoming) {
+        //                 console.log('Attribute', existing, incoming);
+        //                 return { ...existing, ...incoming };
+        //             },
+        //         },
+        //     },
+        // },
+
+    },
+});
 
 export const client = new ApolloClient({
     uri: "http://localhost:4000/",
-    cache: new InMemoryCache()
+    cache: cache
 });
 
