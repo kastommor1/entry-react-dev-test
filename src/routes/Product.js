daf4c8b6f2ab0572e908abc52f161f4b15a5ddc4
kastommor1 <kastommor@gmail.com>
2022-07-19 10:28:20 +0300

    Product clear

--- src/routes/Product.js
+++ src/routes/Product.js
@@ -121,7 +121,7 @@ class Product extends React.Component {
 
         if (!this.state.product) return (<div></div>);       
 
-        const { id, name, inStock, gallery, prices, brand,  attributes } = this.state.product;
+        const { id, name, gallery, prices, brand,  attributes } = this.state.product;
         document.title = brand + ' ' + name;
 
         return (
@@ -197,120 +197,6 @@ function setHashId(product) {
     return selectedProduct;
 }
 
-
-//-----------------------------------------------------------------
-
-
-class Product1 extends React.Component {
-
-    getProductQuery() {
-        const { query, cart, productId } = this.props;
-        const cartProduct = cart.find(product => product.id === productId);
-
-        if (cartProduct) { return }//check product in cart     
-
-        const { loading, error, data } = query;
-
-        if (loading) return (<Loading />);
-        if (error) return (<WarningMessage><p>Error. {error.message}</p></WarningMessage>);
-        if (!data || !data.product) return (
-            <WarningMessage>
-                <h2>Sorry.</h2>
-                <p>This product does not exist.</p>
-            </WarningMessage>);
-
-        const product = data.product;
-        this.props.onAddToCart(product, true) //add widh 0 quantity                    
-    }
-
-    removePreviewProduct() {
-        const id = this.props.productId;
-        const cart = this.props.cart;
-        const cartProduct = cart.find(product => product.id === id);
-        const filteredCart = cart.filter((product) => product.id !== id);
-
-        if (cartProduct && cartProduct.quantity === 0) {
-            localStorage.setItem('cart', JSON.stringify(filteredCart));
-            this.props.onDeleteFromCart(id);
-        }
-    }
-
-    setDescription() {
-        const { cart, productId } = this.props;
-        const cartProduct = cart.find(product => product.id === productId);
-        if (cartProduct) {
-            let descriptionDOM = document.querySelector('.description');
-            descriptionDOM.innerHTML = cartProduct.description;
-        }
-    }
-
-    componentDidMount() {
-        this.removePreviewProduct();
-        window.addEventListener("beforeunload", this.removePreviewProduct);//Don work 
-
-        this.setDescription(); //or use html-react-parser library
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("beforeunload", this.removePreviewProduct);
-        this.removePreviewProduct();
-    }
-
-    componentDidUpdate() {
-        this.getProductQuery();
-
-        this.setDescription(); //or use html-react-parser library
-    }
-
-
-    render() {
-        const { cart, productId } = this.props;
-        const cartProduct = cart.find(product => product.id === productId);
-
-        if (!cartProduct) { return (<div></div>) }
-
-        const { id, name, inStock, gallery, prices, brand, quantity, attributes } = cartProduct;
-        document.title = brand + ' ' + name;
-
-
-
-        return (
-            <div className="product">
-
-                <GalleryWithIcons gallery={gallery} name={name} />
-
-                <div className="wrapper">
-                    <div className="parameters">
-
-                        <h3 className="brand" ><b>{brand}</b></h3>
-                        <p className="name" >{name}</p>
-
-                        <Attributes
-                            productId={id}
-                            attributes={attributes}
-                            onAttributeChange={this.props.onAttributeChange}
-                        />
-
-                        <p className="price-name">Price:</p>
-                        <Price prices={prices} currentCurrency={this.props.currentCurrency} />
-
-                        <AddButtonBig
-                            inStock={inStock}
-                            quantity={quantity}
-                            product={cartProduct}
-                            onAddToCart={this.props.onAddToCart}
-                            onDeleteFromCart={this.props.onDeleteFromCart}
-                        />
-
-                        <div className="description"></div>
-
-                    </div>
-                </div>
-            </div>
-        )
-
-    }
-}
 
 const ProductHOC = widthProductQueryByParams(Product, GET_PRODUCT, 'productId');
 
